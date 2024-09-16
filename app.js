2eb6df52b82ac40e845fc810c19812a448cb16bc
ricardoazcosta <ricardozanete@gmail.com>
2024-09-16 09:34:22 -0300

    :package: build: add package inquirer

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const start = () => {
     while (true) {
-        let opcao = 'cadastrar'
-        switch(opcao){
+        let opcao = 'sair'
+        switch (opcao) {
             case 'cadastrar':
-                console.log('vamos cadastrar')
+                console.log('Vamos cadastrar')
+                
                 break;
+
                 case 'listar':
-                    console.log('vamos listar')
-                    break;
-                    case 'sair':
-                        return
+                    console.log('Vamos listar')
+                break;
+                
+                case 'sair':
+                    return
+            
         }
-        
     }
-
-
 }
 
 start()
